c17dfd03e1f8ccc42d276de9b16b6c981143e37c
tahe-ahmed <tahe9598@gmail.com>
2023-10-28 23:49:35 +0200

    feat: add the login wrap function

--- finance_app/utils.py
+++ finance_app/utils.py
@@ -1,4 +1,3 @@
-
 from flask import redirect, render_template, request, session
 from functools import wraps
 
@@ -20,4 +19,17 @@ def apology(message, code=400):
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
