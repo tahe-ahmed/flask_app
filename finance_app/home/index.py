8fee550bb8436e44a35d58c2219360acceb07f57
tahe-ahmed <tahe9598@gmail.com>
2023-10-29 17:35:41 +0100

    fix: improve code style index.py

--- finance_app/home/index.py
+++ finance_app/home/index.py
@@ -1,32 +1,23 @@
 from flask import Blueprint, render_template, session
-from finance_app.db import get_db
-from finance_app.utils import login_required, lookup, usd, unique_stocks
+from finance_app.db import get_database_connection
+from finance_app.utils import check_login, format_currency
 
-portfolio_blueprint = Blueprint('portfolio', __name__,  template_folder='templates', url_prefix='')
+portfolio_blueprint = Blueprint('portfolio', __name__, template_folder='templates', url_prefix='')
+
+INITIAL_CASH = 10000
 
 @portfolio_blueprint.route("/")
-@login_required
-def index():
-    """Show portfolio of stocks"""
-
-    db = get_db()
-
-    # Fetch buy stocks from transactions
-    stocks = get_buy_stocks(db)
-
-    # Calculate total holdings for each symbol
-    symbol_totals = calculate_symbol_totals(stocks)
-
-    # Create a list of dictionaries for filtered data
-    filtered_data = create_filtered_data(symbol_totals)
-
-    cash = 10000
+@check_login
+def show_portfolio():
+    db = get_database_connection()
+    buy_transactions = fetch_buy_transactions(db)
+    symbol_totals = calculate_holding_totals(buy_transactions)
+    filtered_data = prepare_filtered_data(symbol_totals)
+    cash = INITIAL_CASH
     available_cash = calculate_available_cash(cash, symbol_totals)
+    return render_template("index.html", stocks=filtered_data, available_cash=format_currency(available_cash), cash=format_currency(cash))
 
-    return render_template("index.html", stocks=filtered_data, available_cash=usd(available_cash), cash=usd(cash))
-
-def get_buy_stocks(db):
-    """Fetch buy stocks from transactions for the user"""
+def fetch_buy_transactions(db):
     query = """
         SELECT symbol, shares, price
         FROM transactions
@@ -34,21 +25,18 @@ def get_buy_stocks(db):
     """
     return db.execute(query, (int(session['user_id']),)).fetchall()
 
-def calculate_symbol_totals(stocks):
-    """Calculate total shares and total price for each symbol"""
+def calculate_holding_totals(buy_transactions):
     symbol_totals = {}
-    for symbol, shares, price in stocks:
+    for symbol, shares, price in buy_transactions:
         if symbol not in symbol_totals:
-            symbol_totals[symbol] = {'shares': 0, 'price': 0}
+            symbol_totals[symbol] = {'shares': 0, 'total_price': 0}
         symbol_totals[symbol]['shares'] += shares
-        symbol_totals[symbol]['price'] += shares * price
+        symbol_totals[symbol]['total_price'] += shares * price
     return symbol_totals
 
-def create_filtered_data(symbol_totals):
-    """Create a list of dictionaries for filtered data"""
-    return [{'symbol': symbol, 'name': symbol, 'shares': totals['shares'], 'price': round(totals['price'], 2), 'total': round(totals['price'], 2)}
+def prepare_filtered_data(symbol_totals):
+    return [{'symbol': symbol, 'name': symbol, 'shares': totals['shares'], 'price': round(totals['total_price'], 2), 'total': round(totals['total_price'], 2)}
             for symbol, totals in symbol_totals.items()]
 
 def calculate_available_cash(cash, symbol_totals):
-    """Calculate available cash for the user"""
-    return cash - sum(totals['price'] for totals in symbol_totals.values())
+    return cash - sum(totals['total_price'] for totals in symbol_totals.values())
