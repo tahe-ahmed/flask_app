5bea70ff48bde8898fda1957fd9639a7c4832636
tahe-ahmed <tahe9598@gmail.com>
2023-10-31 00:47:56 +0100

    feat: improve index page logic to handle sell functionality

--- finance_app/home/index.py
+++ finance_app/home/index.py
@@ -10,33 +10,52 @@ INITIAL_CASH = 10000
 @login_required
 def show_portfolio():
     db = get_db()
-    buy_transactions = fetch_buy_transactions(db)
-    symbol_totals = calculate_holding_totals(buy_transactions)
-    filtered_data = prepare_filtered_data(symbol_totals)
-    cash = INITIAL_CASH
-    available_cash = calculate_available_cash(cash, symbol_totals)
-    return render_template("index.html", stocks=filtered_data, available_cash=usd(available_cash), cash=usd(cash))
 
-def fetch_buy_transactions(db):
+    # Retrieve buy and sell transactions
+    buy_transactions = get_user_transactions(db, session['user_id'], 'BUY')
+    sell_transactions = get_user_transactions(db, session['user_id'], 'SELL')
+
+    # Calculate total holdings
+    total_holdings = calculate_holdings(buy_transactions, sell_transactions)
+
+    # Calculate available cash
+    available_cash = calculate_available_cash(INITIAL_CASH, total_holdings)
+
+    return render_template("index.html", stocks=total_holdings, available_cash=usd(available_cash), cash=usd(INITIAL_CASH))
+
+def get_user_transactions(db, user_id, operation_name):
     query = """
         SELECT symbol, shares, price
         FROM transactions
-        WHERE user_id = ? AND operation_name = 'BUY'
+        WHERE user_id = ? AND operation_name = ?
     """
-    return db.execute(query, (int(session['user_id']),)).fetchall()
+    return db.execute(query, (user_id, operation_name)).fetchall()
 
-def calculate_holding_totals(buy_transactions):
+def calculate_holdings(buy_transactions, sell_transactions):
     symbol_totals = {}
+    
+    # Calculate buy totals
     for symbol, shares, price in buy_transactions:
         if symbol not in symbol_totals:
-            symbol_totals[symbol] = {'shares': 0, 'total_price': 0}
+            symbol_totals[symbol] = {'shares': 0, 'total_price': 0, 'price_per_share': price}
         symbol_totals[symbol]['shares'] += shares
         symbol_totals[symbol]['total_price'] += shares * price
+
+    # Subtract sell totals
+    for symbol, shares, price in sell_transactions:
+        if symbol in symbol_totals:
+            symbol_totals[symbol]['shares'] -= shares
+            symbol_totals[symbol]['total_price'] -= shares * price
+
+    # Filter out stocks with zero shares
+    symbol_totals = {symbol: data for symbol, data in symbol_totals.items() if data['shares'] > 0}
+    # symbol_totals.update( , total_price)
+    
+    for symbol_to_update in symbol_totals:
+        symbol_totals[symbol_to_update]['total_price'] = usd(symbol_totals[symbol_to_update]['total_price'])
+    
     return symbol_totals
 
-def prepare_filtered_data(symbol_totals):
-    return [{'symbol': symbol, 'name': symbol, 'shares': totals['shares'], 'price': usd(totals['total_price']), 'total': usd(totals['total_price'])}
-            for symbol, totals in symbol_totals.items()]
-
-def calculate_available_cash(cash, symbol_totals):
-    return cash - sum(totals['total_price'] for totals in symbol_totals.values())
+def calculate_available_cash(initial_cash, holdings):
+    used_cash = sum( float(data['total_price'][1:]) for data in holdings.values())
+    return initial_cash - used_cash
